34a1c0965856000caaad5994691cc7c4d56bf7ab
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-09-29 10:49:17 +0700

    create table nilai

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 from flask.app import Flask
 from flask.templating import render_template
 from flask import request
-from sql_helper import select_nilai, detail_siswa
-
+from sql_helper import detail_siswa
 app = Flask(__name__)
 
-rows = []
-res = select_nilai()
-mapel = ["Matematika", "Fisika", "Biologi", "Bahasa Indonesia", "Bahasa Inggris", "Sejarah", "Kimia"]
-for i in range(len(res)//7):
-    nama, nrp, kota = (detail_siswa(i + 1))
-    nrp = nrp.rjust(14, '0')
-    rows.append([nama, nrp, kota])
-    row = res[i:i+7]
-    _, nilai, _ = zip(*row)
-    nilai = list(map(float, nilai))
-    rows[i].append(nilai)
+res = detail_siswa()
 
 @app.route("/")
 def index():
     page = request.args.get('page', 0, type=int)
-    return render_template('index.html', rows=rows, mapel=mapel, enumerate=enumerate, page=page)
+    return render_template('index.html', res=res, enumerate=enumerate, page=page)
--- sql_helper.py
+++ sql_helper.py
@@ -37,7 +37,7 @@ def select_nilai():
     except Error as e:
         print("Error while connecting to MySQL", e)
 
-def detail_siswa(id_siswa):
+def detail_siswa():
     try:
         conn = msql.connect(host='localhost', user='root', password='')
         if conn.is_connected():
@@ -49,8 +49,8 @@ def detail_siswa(id_siswa):
         conn = msql.connect(host='localhost', database='kluster', user='root', password='')
         if conn.is_connected():
             cursor = conn.cursor()
-            cursor.execute(f"SELECT `siswa`.`nama_siswa`, `siswa`.`nisn`, `kota`.`nama_kota` FROM `siswa` INNER JOIN `kota` ON `siswa`.`id_kota` = `kota`.`id_kota` WHERE `siswa`.`id_siswa` = {id_siswa};")
-            record = cursor.fetchone()
+            cursor.execute(f"SELECT `nilai`.`id_siswa`,`siswa`.`nama_siswa`,`siswa`.`nisn`,`kota`.`nama_kota`,MAX(CASE WHEN `nilai`.`id_mapel`='1' THEN `nilai`.`nilai` END) 'Matematika',MAX(CASE WHEN `nilai`.`id_mapel`='2' THEN `nilai`.`nilai` END) 'Fisika',MAX(CASE WHEN `nilai`.`id_mapel`='3' THEN `nilai`.`nilai` END) 'Biologi',MAX(CASE WHEN `nilai`.`id_mapel`='3' THEN `nilai`.`nilai` END) 'Bahasa Indonesia',MAX(CASE WHEN `nilai`.`id_mapel`='3' THEN `nilai`.`nilai` END) 'Bahasa Inggris',MAX(CASE WHEN `nilai`.`id_mapel`='3' THEN `nilai`.`nilai` END) 'Sejarah',MAX(CASE WHEN `nilai`.`id_mapel`='3' THEN `nilai`.`nilai` END) 'Kimia' FROM nilai INNER JOIN `siswa` ON `siswa`.`id_siswa` = `nilai`.`id_siswa` INNER JOIN `kota` ON `kota`.`id_kota`= `siswa`.`id_kota` GROUP BY `nilai`.`id_siswa`;")
+            record = cursor.fetchall()
             return record
     except Error as e:
         print("Error while connecting to MySQL", e)
